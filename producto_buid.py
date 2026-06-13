6e93794275f41531c5205cca513569d0964707d1
FernandoJ2006 <233383380+fernaj2006@users.noreply.github.com>
2026-06-13 17:41:36 -0400

    Archivo Buid listo, ahora toca implementerlo a software principal

---
+++ producto_buid.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+# Interfaz de que define los métodos para construir un producto generico
+
+class IbuilderProducto(ABC):
+    
+    @abstractmethod
+    def set_nombre(self, nombre: str) -> IbuilderProducto:
+        pass
+    
+    @abstractmethod
+    def set_cantidad(self, cantidad: int) -> IbuilderProducto:
+        pass
+    
+    @abstractmethod
+    def set_precio(self, precio: float) -> IbuilderProducto:
+        pass
+    
+    @abstractmethod
+    def set_codigo(self, codigo: str) -> IbuilderProducto:
+        pass
+    
+    @abstractmethod
+    def build(self) -> dict:
+        pass
+    
+class BuilderProducto(IbuilderProducto):
+    
+    def __init__(self):
+        self.producto = {}
+    
+    def set_nombre(self, nombre: str) -> BuilderProducto:
+        self.producto['producto'] = nombre
+        return self
+    
+    def set_cantidad(self, cantidad: int) -> BuilderProducto:
+        self.producto['cantidad'] = cantidad
+        return self
+    
+    def set_precio(self, precio: float) -> BuilderProducto:
+        self.producto['precio'] = precio
+        return self
+
+    def set_codigo(self, codigo: str) -> BuilderProducto:
+        self.producto['codigo'] = codigo
+        return self
+
+    def build(self):
+        campo_requerido = ['producto', 'cantidad', 'precio', 'codigo']
+        for campo in campo_requerido:
+            if campo not in self.producto:
+                raise ValueError(f"Falta el campo requerido: {campo}")
+            
+        return self.producto
